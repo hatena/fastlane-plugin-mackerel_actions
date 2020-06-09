13f85ce876b7708e480dca174fb57b18e9be9a86
yutailang0119 <muta.yutaro@gmail.com>
2020-06-09 20:31:51 +0900

    Remove mackerel_actions_action and related

--- spec/mackerel_actions_action_spec.rb
+++
@@ -1,9 +0,0 @@
-describe Fastlane::Actions::MackerelActionsAction do
-  describe '#run' do
-    it 'prints a message' do
-      expect(Fastlane::UI).to receive(:message).with("The mackerel_actions plugin is working!")
-
-      Fastlane::Actions::MackerelActionsAction.run(nil)
-    end
-  end
-end
