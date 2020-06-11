b988d5ab7fd8c99e965e52eaad34956c88b495bc
yutailang0119 <muta.yutaro@gmail.com>
2020-06-11 13:37:57 +0900

    Add SampleApp project

---
+++ examples/SampleApp/SampleAppTests/SampleAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  SampleAppTests.swift
+//  SampleAppTests
+//
+//  Created by Yutaro Muta on 2020/06/11.
+//  Copyright © 2020 yutailang0119. All rights reserved.
+//
+
+import XCTest
+@testable import SampleApp
+
+class SampleAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
